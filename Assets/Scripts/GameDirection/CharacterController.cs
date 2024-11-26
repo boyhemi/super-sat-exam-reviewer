48191136d009937acd4ea0da8b35d1d0e9ca0307
Basti Arcega <41557188+boyhemi@users.noreply.github.com>
2024-11-26 19:45:03 +0800

    [FEAT] Added initial game over sequence animation

---
+++ Assets/Scripts/GameDirection/CharacterController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public AnimatorController characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
